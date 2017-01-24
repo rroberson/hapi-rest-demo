7eae5f25179c11c78cbae2da2a85003e206fc95c
Russ Roberson <russ.roberson00@gmail.com>
2017-01-24 07:48:40 -0600

    changed sqitch: not add column, API commented out: findByName

--- src/routes/product.js
+++ src/routes/product.js
@@ -10,11 +10,14 @@ module.exports = function () {
             path: '/products/{product_id}',
             handler: productController.findByID
         },
-         {
-             method: 'GET',
-             path: '/products/title/{title}',
-             handler: productController.findByName
-        },
+	// My notes: From his Github, this was not commented out (representing final state), but I am commenting 
+	//           out initially, to be more like his overall flow, where in the video, it was commented because
+	//           he uncomments it at the end to use Github changes triggering new flows, etc.
+        // {
+        //     method: 'GET',
+        //     path: '/products/title/{title}',
+        //     handler: productController.findByName
+        // },
         {
             method: 'GET',
             path: '/products',
